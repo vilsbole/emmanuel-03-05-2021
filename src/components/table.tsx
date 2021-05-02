7fbfe25d09ba9211a439a1b140cf4f6cf063e29d
Emmanuel Vilsbol <emmanuel@vilsbol.com>
2021-05-02 23:17:52 +0200

    Type Table

--- src/components/table.tsx
+++ src/components/table.tsx
@@ -1,35 +1,39 @@
 import React from "react";
 import { Box } from "rebass";
 
-export const CellHead = ({ children, ...props }) => (
+export const CellHead: React.FC = ({ children, ...props }) => (
   <Box as="th" {...props}>
     {children}
   </Box>
 );
 
-export const CellData = ({ children, ...props }) => (
+export const CellData: React.FC = ({ children, ...props }) => (
   <Box as="td" {...props}>
     {children}
   </Box>
 );
 
-export const TableRow = ({ children, ...props }) => (
+export const TableRow: React.FC = ({ children, ...props }) => (
   <Box as="tr" {...props}>
     {children}
   </Box>
 );
 
-const TableTag = ({ children, ...props }) => (
+const TableTag: React.FC = ({ children, ...props }) => (
   <Box as="table" height="100%" {...props}>
     {children}
   </Box>
 );
 
-const Table = ({ columns, children, ...props }) => {
+const Table: React.FC<{ columns: string[] }> = ({
+  columns,
+  children,
+  ...props
+}) => {
   return (
     <TableTag {...props}>
       <thead>
-        <tr>{columns.map((c, idx) => c)}</tr>
+        <tr>{columns.map((c) => c)}</tr>
       </thead>
       <tbody>{children}</tbody>
     </TableTag>
